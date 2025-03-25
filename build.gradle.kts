bda7a63b4a0e2c76aab2c58cc29c573fcce9d18b
Dietrich Travkin <dietrich.travkin@solunar.de>
2025-03-25 17:31:45 +0100

    Add gradle tasks for adapting PlantUML lib Eclipse projects
    - update versions to that of latest PlantUML lib release
    - replace jars with that from latest PlantUML lib release

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URI
+import kotlin.io.path.Path
+import kotlin.io.path.name
 
 plugins {
     base
@@ -40,23 +42,168 @@ fun readLatestPlantUmlLibReleaseVersion(): String? {
 
 fun readVersionFromRelease(releaseJson: String): String {
     val json = JsonSlurper().parseText(releaseJson) as Map<*, *>
-    var versionText = json["tag_name"].toString()
-
-    if (versionText.startsWith("v")) {
-        versionText = versionText.substringAfter("v")
-    }
-    return versionText
+    return json["tag_name"].toString()
 }
 
-//val testTask = tasks.register<Exec>("get-version") {
-//    //workingDir = file("../plantuml-eclipse").absoluteFile
-//    commandLine = listOf("", "clean", "package")
-//}
+fun downloadFile(sourceFileUrl : String, destinationDirectoryPath : String) {
+    val uri = URI(sourceFileUrl)
+    val fileName = Path(uri.path).name
+    val destinationFile = File(destinationDirectoryPath, fileName)
+
+    createDirs(destinationFile.parentFile)
+
+    ant.invokeMethod("get", mapOf("src" to sourceFileUrl, "dest" to destinationFile))
+}
+
+// for some reason File.mkDirs() doesn't always work, thus, this work-around is needed
+fun createDirs(directory: File) {
+    if (directory.parentFile != null && !directory.parentFile.exists()) {
+        createDirs(directory.parentFile)
+    }
+    if (!directory.exists()) {
+        directory.mkdir()
+    }
+}
+
+
+val plantUmlLibRootDir = "plantuml-lib"
+val pluginName = "net.sourceforge.plantuml.library"
+val featureName = "$pluginName.feature"
+val plantUmlLibPluginDir = "$plantUmlLibRootDir/$pluginName"
+val plantUmlLibFeatureDir = "$plantUmlLibRootDir/$featureName"
+val plantUmlLibPluginLibDir = "$plantUmlLibPluginDir/lib"
+val latestPlantUmlLibReleaseVersion =  readLatestPlantUmlLibReleaseVersion()
+val latestPlantUmlLibReleaseVersionSimple = latestPlantUmlLibReleaseVersion?.substringAfter("v")
+
+
+val downloadPlantUmlLibsTask = tasks.register("downloadPlantUmlLibs") {
+    group = "plantuml-lib"
+
+    downloadFile("https://github.com/plantuml/plantuml/releases/download/$latestPlantUmlLibReleaseVersion/plantuml-epl-$latestPlantUmlLibReleaseVersionSimple.jar", "build/lib")
+    downloadFile("https://github.com/plantuml/plantuml/releases/download/$latestPlantUmlLibReleaseVersion/plantuml-epl-$latestPlantUmlLibReleaseVersionSimple-sources.jar", "build/lib")
+}
+
+val deleteObsoleteLibsTask = tasks.register<Delete>("clearEclipsePluginLibDir") {
+    group = "plantuml-lib"
+
+    delete(fileTree(plantUmlLibPluginLibDir).include("*.jar"))
+}
+
+val copyLibsTask = tasks.register<Copy>("copyLibsToEclipsePlugin") {
+    group = "plantuml-lib"
+
+    dependsOn(downloadPlantUmlLibsTask)
+    dependsOn(deleteObsoleteLibsTask)
+
+    from("build/lib")
+    into(plantUmlLibPluginLibDir)
+    include("*.jar")
+}
+
+val updateVersionsInManifestTask = tasks.register<Copy>("updateVersionsInManifest") {
+    group = "plantuml-lib"
+
+    from("$plantUmlLibPluginDir/META-INF") {
+        include("MANIFEST.MF")
+        filter { line: String ->
+            if (line.startsWith("Bundle-Version:")) {
+                "Bundle-Version: $latestPlantUmlLibReleaseVersionSimple.qualifier"
+            }
+            else if (line.startsWith("Bundle-ClassPath: lib/plantuml-epl-")) {
+                "Bundle-ClassPath: lib/plantuml-epl-$latestPlantUmlLibReleaseVersionSimple.jar"
+            }
+            else line
+        }
+    }
+    into("build/eclipse-files/$pluginName/META-INF")
+    filteringCharset = "UTF-8"
+}
+
+val updateVersionsInClasspathTask = tasks.register<Copy>("updateVersionsInClasspath") {
+    group = "plantuml-lib"
+
+    val linePrefix = "<classpathentry exported=\"true\" kind=\"lib\" path=\"lib/plantuml-epl-"
+
+    from(plantUmlLibPluginDir) {
+        include(".classpath")
+        filter { line: String ->
+            if (line.contains(linePrefix, false)) {
+                val start = line.indexOf(linePrefix) + linePrefix.length
+                val end = line.indexOf(".jar\"")
+                line.substring(0, start) + latestPlantUmlLibReleaseVersionSimple + line.substring(end)
+            }
+            else line
+        }
+    }
+
+    from(plantUmlLibPluginDir) {
+        include("build.properties")
+        filter { line: String ->
+            if (line.startsWith("bin.includes = lib/plantuml-epl-")) {
+                "bin.includes = lib/plantuml-epl-$latestPlantUmlLibReleaseVersionSimple.jar,\\"
+            }
+            else if (line.startsWith("src.includes = lib/plantuml-epl-")) {
+                "src.includes = lib/plantuml-epl-$latestPlantUmlLibReleaseVersionSimple-sources.jar,\\"
+            }
+            else line
+        }
+    }
+    into("build/eclipse-files/$pluginName")
+    filteringCharset = "UTF-8"
+}
+
+val updateVersionInFeatureTask = tasks.register<Copy>("updateVersionInFeature") {
+    group = "plantuml-lib"
+
+    from(plantUmlLibFeatureDir) {
+        include("feature.xml")
+        filter { line: String ->
+            if (line.trim().startsWith("version=\"") && line.endsWith(".qualifier\"")) {
+                line.substring(0, line.indexOf("\"") + 1) + latestPlantUmlLibReleaseVersionSimple + line.substring(line.indexOf(".qualifier"))
+            }
+            else line
+        }
+    }
+    into("build/eclipse-files/$featureName")
+    filteringCharset = "UTF-8"
+}
+
+val updateVersionInParentPomTask = tasks.register<Copy>("updateVersionInPom") {
+    group = "plantuml-lib"
+
+    val startTag = "<plantuml-lib-version>"
+    val endTag = "</plantuml-lib-version>"
+
+    from(plantUmlLibRootDir) {
+        include("pom.xml")
+        filter { line: String ->
+            if (line.trim().startsWith(startTag) && line.endsWith(endTag)) {
+                line.substring(0, line.indexOf(startTag) + startTag.length) + latestPlantUmlLibReleaseVersionSimple + line.substring(line.indexOf(endTag))
+            }
+            else line
+        }
+    }
+    into("build/eclipse-files")
+    filteringCharset = "UTF-8"
+}
+
+val updateVersionsInEclipseProjectsTask = tasks.register<Copy>("updateVersionsInEclipseProjects") {
+    group = "plantuml-lib"
+
+    dependsOn(updateVersionsInManifestTask)
+    dependsOn(updateVersionsInClasspathTask)
+    dependsOn(updateVersionInFeatureTask)
+    dependsOn(updateVersionInParentPomTask)
+
+    from("build/eclipse-files")
+    into(plantUmlLibRootDir)
+    filteringCharset = "UTF-8"
+}
 
 tasks.register("printLatestPlantUMLVersion") {
-    val latestVersion = readLatestPlantUmlLibReleaseVersion()
+    group = "plantuml-lib"
 
     doLast {
-        println("Latest PlantUML lib version is $latestVersion.")
+        println("Latest PlantUML lib version is $latestPlantUmlLibReleaseVersion.")
     }
 }
