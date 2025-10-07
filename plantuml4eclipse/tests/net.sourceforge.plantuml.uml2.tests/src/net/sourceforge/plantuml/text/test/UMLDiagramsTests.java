b5ed034e27c40d526367812684e861f4cf66af38
Dietrich Travkin <dietrich.travkin@solunar.de>
2025-10-07 18:38:25 +0200

    Fix license hints
    The project uses EPL 1.0, not EPL 2.0

--- plantuml4eclipse/tests/net.sourceforge.plantuml.uml2.tests/src/net/sourceforge/plantuml/text/test/UMLDiagramsTests.java
+++ plantuml4eclipse/tests/net.sourceforge.plantuml.uml2.tests/src/net/sourceforge/plantuml/text/test/UMLDiagramsTests.java
@@ -4,9 +4,9 @@
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License 2.0 which
  * accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-2.0/
+ * https://www.eclipse.org/org/documents/epl-v10.html
  *
- * SPDX-License-Identifier: EPL-2.0
+ * SPDX-License-Identifier: EPL-1.0
  */
 
 package net.sourceforge.plantuml.text.test;
